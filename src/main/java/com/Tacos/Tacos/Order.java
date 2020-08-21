e626955ef0c5065f9f8f1ec3b9236e53e6bb97c2
sanix sandel <sanicksikani@gmail.com>
2020-08-21 19:28:47 +0300

    Order

---
+++ src/main/java/com/Tacos/Tacos/Order.java
@@ -0,0 +1,4 @@
+package com.Tacos.Tacos;
+
+public class Order {
+}
--- src/main/java/com/Tacos/Tacos/controllers/OrderController.java
+++ src/main/java/com/Tacos/Tacos/controllers/OrderController.java
@@ -2,6 +2,7 @@ package com.Tacos.Tacos.controllers;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class OrderController {
 
@@ -9,5 +10,11 @@ public class OrderController {
     public String orderForm(Model model){
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order){
+        log.info("Order submitted: "+order);
+        return "redirect:/";
     }
 }
