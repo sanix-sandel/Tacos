39700223d73ba4bfdd9c9a33cc39fad5b164bf2d
sanix sandel <sanicksikani@gmail.com>
2020-08-24 06:09:23 +0300

    validation

--- src/main/java/com/Tacos/Tacos/controllers/DesignTacoController.java
+++ src/main/java/com/Tacos/Tacos/controllers/DesignTacoController.java
@@ -5,11 +5,13 @@ import com.Tacos.Tacos.models.Ingredient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,9 +53,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(@ModelAttribute("design") Taco design){
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors){
 
-
+        if(errors.hasErrors()){
+            return "design";
+        }
         //log.info("Process design: "+design);
         return "redirect:/orders/current";
     }
