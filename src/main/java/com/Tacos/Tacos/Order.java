2f433ac60d09665a906d80e68fdf5aea998060a2
sanix sandel <sanicksikani@gmail.com>
2020-08-24 11:06:53 +0300

    JPA- Annotating domains as Entities

--- src/main/java/com/Tacos/Tacos/Order.java
+++ src/main/java/com/Tacos/Tacos/Order.java
@@ -2,6 +2,7 @@ package com.Tacos.Tacos;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -10,8 +11,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Entity
+@Table(name="Taco_Order")
 public class Order {
 
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
     private Date placedAt;
@@ -41,9 +48,15 @@ public class Order {
     @Digits(integer=3, fraction=0, message="Invalid CCV")
     private String ccCVV;
 
+    @ManyToMany(targetEntity=Taco.class)
     private List<Taco> tacos=new ArrayList<>();
 
     public void addDesign(Taco design){
         this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt(){
+        this.placedAt=new Date();
     }
 }
